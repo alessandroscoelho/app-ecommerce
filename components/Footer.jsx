4fee641579c6a5879b6fb3d8de4d1780e6c572d2
alessandroscoelho <alessandroscoelho@gmail.com>
2025-11-20 09:30:28 -0300

    outros ajustes

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -9,16 +9,14 @@ const Footer = () => {
         <div className="w-4/5">
           <Image className="w-28 md:w-32" src={assets.logo} alt="logo" />
           <p className="mt-6 text-sm">
-            Lorem Ipsum is simply dummy text of the printing and typesetting
-            industry. Lorem Ipsum has been the industry's standard dummy text
-            ever since the 1500s, when an unknown printer took a galley of type
-            and scrambled it to make a type specimen book.
+            Somos uma startup de tecnologia que temos paixão por eficiencia e simplicidade. A 4 anos no mercado temos nos destacado pela eficiencia em atender nossos clientes de forma 
+            honesta e verdadeira.
           </p>
         </div>
 
         <div className="w-1/2 flex items-center justify-start md:justify-center">
           <div>
-            <h2 className="font-medium text-gray-900 mb-5">Company</h2>
+            <h2 className="font-medium text-gray-900 mb-5">Empresa</h2>
             <ul className="text-sm space-y-2">
               <li>
                 <a className="hover:underline transition" href="#">Home</a>
@@ -41,13 +39,13 @@ const Footer = () => {
             <h2 className="font-medium text-gray-900 mb-5">Entre em contato</h2>
             <div className="text-sm space-y-2">
               <p>+1-234-567-890</p>
-              <p>contato@sof7wares.com</p>
+              <p>contato@sof7ware.com</p>
             </div>
           </div>
         </div>
       </div>
       <p className="py-4 text-center text-xs md:text-sm">
-        Copyright 2025 © Sof7wares. All Right Reserved.
+        Copyright 2025 © Sof7ware.com - All Right Reserved.
       </p>
     </footer>
   );
--- components/seller/Sidebar.jsx
+++ components/seller/Sidebar.jsx
@@ -7,9 +7,9 @@ import { usePathname } from 'next/navigation';
 const SideBar = () => {
     const pathname = usePathname()
     const menuItems = [
-        { name: 'Add Product', path: '/seller', icon: assets.add_icon },
-        { name: 'Product List', path: '/seller/product-list', icon: assets.product_list_icon },
-        { name: 'Orders', path: '/seller/orders', icon: assets.order_icon },
+        { name: 'Adicionar produto', path: '/seller', icon: assets.add_icon },
+        { name: 'Listar produtos', path: '/seller/product-list', icon: assets.product_list_icon },
+        { name: 'Pedidos', path: '/seller/orders', icon: assets.order_icon },
     ];
 
     return (
