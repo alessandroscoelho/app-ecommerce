d3cc75210e835c71b6ae7858e30bf13bd09f8d07
alessandroscoelho <alessandroscoelho@gmail.com>
2025-11-20 12:08:48 -0300

    update database

--- config/inngest.js
+++ config/inngest.js
@@ -7,9 +7,7 @@ export const inngest = new Inngest({ id: "quickcart-next" });
 
 // Inngets Funcion to save user adta to a database
 export const syncUserCreation = inngest.createFunction(
-    {
-        id: 'sync-user-from-clerk'
-    },
+    { id: 'sync-user-from-clerk' },
     { event: 'clerk/user.created' },
     async ({ event }) => {
         const { id, first_name, last_name, email_addresses, image_url } = event.data
