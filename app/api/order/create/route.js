104e18b83c8e0ce636d49188c84b76eedc4bb1ae
alessandroscoelho <alessandroscoelho@gmail.com>
2025-12-09 18:59:26 -0300

    config pedidos

--- app/api/order/create/route.js
+++ app/api/order/create/route.js
@@ -7,7 +7,7 @@ import { NextResponse } from "next/server";
 export async function POST(request) {
     try {
         
-        const userId = getAuth(request)
+        const { userId } = getAuth(request)
         const { address, items } = await request.json();
 
         if (!address || items.length === 0) {
@@ -17,7 +17,7 @@ export async function POST(request) {
         //calculate amount using items
         const amount = await items.reduce(async (acc, item) => {
             const product = await Product.findById(item.product);
-            return acc + product.offerPrice * item.quantity;
+            return await acc + product.offerPrice * item.quantity;
         },0)
 
          await inngest.send({
