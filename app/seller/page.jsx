af536660282fe4e06230d4eb8e99e8a06c912ea9
alessandroscoelho <alessandroscoelho@gmail.com>
2025-12-30 15:06:13 -0300

    commit

--- app/seller/page.jsx
+++ app/seller/page.jsx
@@ -125,13 +125,13 @@ const AddProduct = () => {
               onChange={(e) => setCategory(e.target.value)}
               defaultValue={category}
             >
-              <option value="Earphone">Earphone</option>
-              <option value="Headphone">Headphone</option>
-              <option value="Watch">Watch</option>
-              <option value="Smartphone">Smartphone</option>
-              <option value="Laptop">Laptop</option>
-              <option value="Camera">Camera</option>
-              <option value="Accessories">Accessories</option>
+              <option value="Vestido">Vestido</option>
+              <option value="Saia">Saia</option>
+              <option value="Calcado">Calçado</option>
+              <option value="Blusa">Blusa</option>
+              <option value="Acessorios">Acessórios</option>
+              <option value="Short">Short</option>
+              <option value="Camisa">Camisa</option>
             </select>
           </div>
           <div className="flex flex-col gap-1 w-32">
